442156f599742da712737905f69e3845a557a89b
EoinFarrell <efarrell@zendesk.com>
2021-05-06 22:46:08 +0100

    clean up

--- public/script.js
+++ public/script.js
@@ -1,22 +1,21 @@
 function formSubmit(event) {
   var url = "https://phrhyp7dx2.execute-api.eu-west-1.amazonaws.com/Production";
+  
   var request = new XMLHttpRequest();
   request.open('POST', url, true);
   request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  request.onload = function() { // request successful
-  // we can use server response to our request now
+  
+  request.onload = function() {
     console.log(request.responseText);
     new Notif("Thank you for signing up and enjoy the next race.", "success").display(3500);
   };
 
   request.onerror = function() {
-    // request failed
     new Notif("Error submitting form, sorry, maybe try again.", "error").display(3500);
   };
 
-  request.send(new FormData(event.target)); // create FormData from form that triggered event
+  request.send(new FormData(event.target));
   event.preventDefault();
 }
-
 
 document.querySelector('form').addEventListener("submit", formSubmit);
