c9fe838fa67f10369f196ec96f543a0bf8091731
EoinFarrell <efarrell@zendesk.com>
2021-05-12 08:03:02 +0100

    Change to ajax request

--- public/script.js
+++ public/script.js
@@ -1,21 +1,25 @@
-function formSubmit(event) {
-  var url = "https://phrhyp7dx2.execute-api.eu-west-1.amazonaws.com/Production";
-  
-  var request = new XMLHttpRequest();
-  request.open('POST', url, true);
-  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  
-  request.onload = function() {
-    console.log(request.responseText);
-    new Notif("Thank you for signing up and enjoy the next race.", "success").display(3500);
-  };
-
-  request.onerror = function() {
-    new Notif("Error submitting form, sorry, maybe try again.", "error").display(3500);
-  };
-
-  request.send(new FormData(event.target));
+async function formSubmit(event) {
   event.preventDefault();
+  
+  let user = {
+    phone: document.querySelector('#phone').value,
+    email: document.querySelector('#email').value,
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+  };
+  
+  let response = await fetch("https://phrhyp7dx2.execute-api.eu-west-1.amazonaws.com/Production", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  });
+
+  if (response.ok) { // if HTTP-status is 200-299
+    new Notif("Thank you for signing up and enjoy the next race.", "success").display(3500);
+  } else {
+    new Notif("Error submitting form, sorry, maybe try again. Debug info: " + response.status, "error").display(3500);
+  }
 }
 
 document.querySelector('form').addEventListener("submit", formSubmit);
